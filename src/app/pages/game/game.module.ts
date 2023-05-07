e2c0b8850f835250a2eed9e0bf121966e0e915ca
Kozma Kristof <bechryko@gmail.com>
2023-05-07 19:49:15 +0200

    Better pipe

--- src/app/pages/game/game.module.ts
+++ src/app/pages/game/game.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game-routing.module';
 import { GameComponent } from './game.component';
-import { ReversePipe } from './reverse-pipe';
-
+import { ReversePipe } from '../../reverse.pipe';
 
 @NgModule({
   declarations: [
