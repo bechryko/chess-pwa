f623fa90279fd61f436d8113034d070976a78891
Kozma Kristof <bechryko@gmail.com>
2023-05-14 01:37:37 +0200

    Environment fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true,
+   firebase: {
+      projectId: 'chess-pwa-b41b8',
+      appId: '1:344576401475:web:73fef7e4626c4f76d1f4f6',
+      storageBucket: 'chess-pwa-b41b8.appspot.com',
+      apiKey: 'AIzaSyCgh3Q92EdDzQ_KscYfvTN8FpXLibWr0zg',
+      authDomain: 'chess-pwa-b41b8.firebaseapp.com',
+      messagingSenderId: '344576401475',
+      measurementId: 'G-RYPCB4DMS5',
+   },
+   production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  firebase: {
-    projectId: 'chess-pwa-b41b8',
-    appId: '1:344576401475:web:73fef7e4626c4f76d1f4f6',
-    storageBucket: 'chess-pwa-b41b8.appspot.com',
-    apiKey: 'AIzaSyCgh3Q92EdDzQ_KscYfvTN8FpXLibWr0zg',
-    authDomain: 'chess-pwa-b41b8.firebaseapp.com',
-    messagingSenderId: '344576401475',
-    measurementId: 'G-RYPCB4DMS5',
-  },
-    production: false,
-    hostUrl: 'http://localhost:4200',
+   firebase: {
+      projectId: 'chess-pwa-b41b8',
+      appId: '1:344576401475:web:73fef7e4626c4f76d1f4f6',
+      storageBucket: 'chess-pwa-b41b8.appspot.com',
+      apiKey: 'AIzaSyCgh3Q92EdDzQ_KscYfvTN8FpXLibWr0zg',
+      authDomain: 'chess-pwa-b41b8.firebaseapp.com',
+      messagingSenderId: '344576401475',
+      measurementId: 'G-RYPCB4DMS5',
+   },
+   production: false,
+   hostUrl: 'http://localhost:4200',
 };
