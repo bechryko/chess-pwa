a6266ec3683589a24bbab33ea8ef3978bde7dd5a
Kristof Kozma <kristof.kozma@casrd.net>
2023-09-01 09:22:26 +0200

    Add onclick username copy

--- src/app/shared/components/user-info/user-info.component.ts
+++ src/app/shared/components/user-info/user-info.component.ts
@@ -1,6 +1,8 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 
@@ -9,7 +11,8 @@ import { UserService } from '../../services/user.service';
    standalone: true,
    imports: [
       CommonModule,
-      MatProgressSpinnerModule
+      MatProgressSpinnerModule,
+      MatSnackBarModule
    ],
    templateUrl: './user-info.component.html',
    styleUrls: ['./user-info.component.scss'],
@@ -23,7 +26,9 @@ export class UserInfoComponent {
    constructor(
       private authService: AuthService, 
       private userService: UserService, 
-      private cdr: ChangeDetectorRef
+      private cdr: ChangeDetectorRef,
+      private clipboard: Clipboard,
+      private copySnackbar: MatSnackBar
    ) { }
 
    ngOnInit(): void {
@@ -45,4 +50,13 @@ export class UserInfoComponent {
          }
       });
    }
+
+   public copyUserName(): void {
+      if(!this.isUserLoggedIn) {
+         return;
+      }
+      this.clipboard.copy(this.username);
+      this.copySnackbar.open("Username copied to clipboard!", "Dismiss", { duration: 2000 });
+   }
+   
 }
