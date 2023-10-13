5edeeca8fb3690824bc57ee4dfb67c4a1b847a5d
Kristof Kozma <kristof.kozma@casrd.net>
2023-10-13 15:29:12 +0200

    Add tests for reverse pipe

--- src/app/shared/pipes/reverse.pipe.spec.ts
+++ src/app/shared/pipes/reverse.pipe.spec.ts
@@ -5,4 +5,24 @@ describe('ReversePipe', () => {
       const pipe = new ReversePipe();
       expect(pipe).toBeTruthy();
    });
+
+   it('can reverse short array', () => {
+      const pipe = new ReversePipe();
+      expect(pipe.transform([1, 2, 3]).toString()).toBe([3, 2, 1].toString());
+   });
+
+   it('can reverse long array', () => {
+      const pipe = new ReversePipe();
+      const array = [];
+      for(let i = 1; i <= 10_000; i++) {
+         array.push(i);
+      }
+      const reversed = [...array].reverse()
+      expect(pipe.transform(array).toString()).toBe(reversed.toString());
+   });
+
+   it('can reverse empty array', () => {
+      const pipe = new ReversePipe();
+      expect(pipe.transform([]).toString()).toBe([].toString());
+   });
 });
