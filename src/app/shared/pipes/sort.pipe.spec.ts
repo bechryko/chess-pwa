be6c5d0a30e0152fe9f88b0cdf31bff94accb32d
Kristof Kozma <kristof.kozma@casrd.net>
2023-10-13 15:36:35 +0200

    Add tests for sort pipe

--- src/app/shared/pipes/sort.pipe.spec.ts
+++ src/app/shared/pipes/sort.pipe.spec.ts
@@ -1,8 +1,22 @@
 import { SortPipe } from './sort.pipe';
 
-xdescribe('SortPipe', () => {
+describe('SortPipe', () => {
    it('create an instance', () => {
       const pipe = new SortPipe();
       expect(pipe).toBeTruthy();
+   });
+
+   it('can sort strings by length', () => {
+      const pipe = new SortPipe();
+      expect(
+         pipe.transform(["apple", "pear", "pineapple", "egg"], 'length')?.toString()
+      ).toBe(
+         ["egg", "pear", "apple", "pineapple"].toString()
+      );
+   });
+
+   it('can handle null value', () => {
+      const pipe = new SortPipe();
+      expect(pipe.transform(null, 'some string')).toBeNull();
    });
 });
