e562fbad9e2e727f68ebcfd07a5327fa6c8bed8c
Kristof Kozma <kristof.kozma@casrd.net>
2023-10-18 08:09:58 +0200

    Add tests for not found component

--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { RouteUrls } from 'src/app/shared/enums/routes';
 import { NotFoundComponent } from './not-found.component';
 
-xdescribe('NotFoundComponent', () => {
+describe('NotFoundComponent', () => {
    let component: NotFoundComponent;
    let fixture: ComponentFixture<NotFoundComponent>;
 
    beforeEach(async () => {
       await TestBed.configureTestingModule({
-         declarations: [NotFoundComponent]
-      })
-         .compileComponents();
+         imports: [
+            NotFoundComponent
+         ],
+         providers: [
+            provideRouter([{
+               path: '**',
+               component: NotFoundComponent
+            }])
+         ]
+      }).compileComponents().then(async () => {
+         const harness = await RouterTestingHarness.create();
+         component = await harness.navigateByUrl('', NotFoundComponent);
+      });
 
       fixture = TestBed.createComponent(NotFoundComponent);
-      component = fixture.componentInstance;
       fixture.detectChanges();
    });
 
    it('should create', () => {
       expect(component).toBeTruthy();
    });
+
+   it('should navigate back to the menu', fakeAsync(() => {
+      const linkPDe = fixture.debugElement.query(By.css("p.link"));
+      linkPDe.triggerEventHandler('click', { button: 0 });
+      tick();
+      expect(location.pathname).toContain(RouteUrls.MENU);
+   }));
 });
